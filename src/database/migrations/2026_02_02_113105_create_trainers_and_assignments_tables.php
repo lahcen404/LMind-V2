0efbd202331c65e6b86bb603038ae2c8a9ac6d3c
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-02 12:33:35 +0100

    create migration trainer & pivoot table

---
+++ src/database/migrations/2026_02_02_113105_create_trainers_and_assignments_tables.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Trainners table
+
+        Schema::create('trainers', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        $table->timestamps();
+    });
+
+    // piivot table
+    
+    Schema::create('trainer_class', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('trainer_id')->constrained('trainers')->onDelete('cascade');
+        $table->foreignId('training_class_id')->constrained('training_classes')->onDelete('cascade');
+        $table->timestamps();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainers_and_assignments_tables');
+    }
+};
