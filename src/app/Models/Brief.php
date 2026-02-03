4eecc896af1269bc0db1c84584c35d1c4745c2d5
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-03 11:05:53 +0100

    create Models (Brief with Relations)

--- src/app/Models/Brief.php
+++ src/app/Models/Brief.php
@@ -7,9 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Brief extends Model
 {
-    protected function casts(): array{
-        return [
-            'type' => BriefType::class,
-        ];
+    
+    protected $fillable = ['title', 'description', 'type', 'sprint_id'];
+
+    protected function casts(): array {
+        return ['type' => BriefType::class]; 
+    }
+
+    public function skills() {
+        return $this->belongsToMany(Skill::class, 'brief_skill')
+            ->withPivot('expected_level'); 
     }
 }
