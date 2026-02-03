507ecaadedf90fe4304dbc65f9b907ec5a685083
lahcen404 <lahcen.maskour2003@gmail.com>
2026-02-03 11:33:56 +0100

    create Model Brief  with Relations

--- src/app/Models/Sprint.php
+++ src/app/Models/Sprint.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sprint extends Model
 {
-    //
+    protected $fillable = ['name','duration','order_sprint','training_class_id'];
+
+    // relations 
+    public function trainingClass(){
+        return $this->belongsTo(TrainingClass::class);
+    }
+
+    public function briefs(){
+        return $this->hasMany(Brief::class);
+    }
 }
